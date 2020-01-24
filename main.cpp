5f5ae3849140a38585ac7e5056c73c70ff9e54a8
Kuba7331 <kuba@kamionka.eu>
2020-01-24 17:52:46 +0100

    First version of my Attack system

---
+++ main.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+#include <windows.h>
+#include <cstdlib>
+#include <stdio.h>
+using namespace std;
+
+void attackSystem(){
+bool ifMissed;
+char wybor;
+char attack, miss;
+int atk,hp,def, enAtk, enHp, enDef,time;
+atk = 10;
+hp  = 100;
+def = 5;
+enAtk = 12;
+enHp = 120;
+enDef = 5;
+time = 0;
+cout<<"HEY WATCH OUT, ENEMY IS JUMPING AT YOU!"<<endl;
+cout<<"Try to attack enemy with trying to press -> a button!"<<endl;
+cin>>attack;
+if(attack!='a')
+{
+    cout<<"You've picked wrong button!"<<endl;
+    hp=hp-enAtk+def;
+    cout<<"Your hp status is: "<<hp<<endl;
+}else{
+    cout<<"You hit the enemy!"<<endl;
+    cout<<"Your hp status is: "<<hp<<endl;
+    enHp=enHp - atk + enDef;
+    cout<<"Enemy health status is: "<<enHp<<endl;
+    Sleep(3000);
+}
+    cout<<"Ok, if you're now alright after this attack, try to miss the enemy attack, by clicking -> s button!"<<endl;
+    Sleep(3000);
+    cout<<"ATTACK INCOMING IN 3 SEC:"<<endl;
+    Sleep(1000);
+    cin>>miss;
+    for(int x=0;x>=3;x++){
+        Sleep(1000);
+        cout<<x<<"..."<<endl;
+        time++;
+    }
+    time=0;
+    if(time<=3 && miss=='s' ){
+        cout<<"Congrats, you've missed the attack, now try again!"<<endl;
+        cout<<"Your hp status is: "<<hp<<endl;
+        cout<<"Enemy health status is: "<<enHp<<endl;
+    }else{
+      cout<<"You failed! Try again!"<<endl;
+      hp=hp-enAtk+def;
+      cout<<"Your hp status is: "<<hp<<endl;
+      cout<<"Enemy health status is: "<<enHp<<endl;
+    }
+    cout<<"Now, let's give it a bit more speed! :) "<<endl;
+    while(hp>0||enHp<0){
+    cout<<"a - Attack"<<endl;
+    cout<<"s - Try to miss"<<endl;
+    cin>>wybor;
+    switch(wybor){
+    case 'a':
+        cout<<"You hit the enemy!"<<endl;
+    cout<<"Your hp status is: "<<hp<<endl;
+    enHp=enHp - atk + enDef;
+    cout<<"Enemy health status is: "<<enHp<<endl;
+    cout<<"ATTACK INCOMING!!"<<endl;
+    break;
+    case 's':
+        cout<<"You've missed the enemy attack, now try to hit him!"<<endl;
+        cout<<"Your hp status is: "<<hp<<endl;
+        cout<<"Enemy health status is: "<<enHp<<endl;
+        break;
+    default:
+        cout<<"You've got hit!"<<endl;
+    cout<<"Your hp status is: "<<hp<<endl;
+    hp=hp - enAtk + def;
+    cout<<"Enemy health status is: "<<enHp<<endl;
+     break;
+    }
+   if(enHp<=0){
+    cout<<"Congratulations! You've beaten the boss!"<<endl;
+    getchar();
+    exit(0);
+   } if (hp<=0){
+    cout<<"OH, you lost, what a pity! :c "<<endl;
+     getchar();
+    exit(0);
+   }
+}
+}
+
+int main()
+{
+    attackSystem();
+    return 0;
+}
